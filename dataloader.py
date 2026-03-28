15b6873a6f2a9edad0dbf057ad2fea439f78ed02
cooler9190 <nikk.georgiev@abv.bg>
2026-03-28 11:45:52 +0100

    Code snippet
    Code snippet from assignment 4 for data loader and visualizer

---
+++ dataloader.py
@@ -0,0 +1,8 @@
+# Code snippet from assignment 4
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("andrewmvd/dog-and-cat-detection")
+
+print("Path to dataset files:", path)
